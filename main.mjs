7c7b030e15e47bf74bf69bf203c7cded6cb8f6e5
neolao <contact@neolao.com>
2024-03-04 23:52:00 +0100

    up

---
+++ main.mjs
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from "electron";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, "preload.mjs"),
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+});
